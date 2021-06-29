bb3dd863095d9525975150facb9c07c452c7ed34
ben-hodgson <bthodgson@gmail.com>
2021-06-29 20:20:19 +0100

    Hello World update

---
+++ Hello_World.java
@@ -0,0 +1,9 @@
+public class Hello_World {
+    /* This is my first Java program
+     * It will print 'Hello World!' as the output
+     */
+    
+     public static void main(String []args) {
+         System.out.println("Hello World!");
+     }
+}
